f341d73c2bf171eb1b873f9a489ce8a670551517
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-02-05 22:06:04 +0900

    無限スクロール

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 (() => {
 
-  const contents = document.querySelector('.contents');
+    const contents = document.getElementById('contents');
 
-  // 
-  const infiniteScrollObserver = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-          if ( ! entry.isIntersecting ) return;
+    // 
+    const infiniteScrollObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if ( ! entry.isIntersecting ) return;
 
-          infiniteScrollObserver.unobserve(entry.target);
-          loadContent();
+            infiniteScrollObserver.unobserve(entry.target);
+            loadContent();
 
-      });
-  });
+        });
+    });
 
-  // 
-  let i = 0;
-  const max = 30;
+    // 
+    let i = 0;
+    const max = 30;
 
-  const loadContent = async () => {
+    const loadContent = async () => {
 
-      const response = await fetchDummy('https://example.com/load?i=' + i);
+        const response = await fetchDummy('https://example.com/load?i=' + i);
 
-      contents.insertAdjacentHTML('beforeend',
-              '<div>' +
-              '#' + (i + 1) + '<br>' +
-              await response.text() +
-              '</div>');
+        contents.insertAdjacentHTML('beforeend',
+                '<div>' +
+                '#' + (i + 1) + '<br>' +
+                await response.text() +
+                '</div>');
 
-      // 
-      i++;
+        // 
+        i++;
 
-      if ( i < max ) infiniteScrollObserver.observe(contents.lastElementChild);
+        if ( i < max ) infiniteScrollObserver.observe(contents.lastElementChild);
 
-  };
+    };
 
-  // 
-  loadContent();
+    // 
+    loadContent();
 
 })();
